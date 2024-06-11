a1e6a6a1743f056d478651e4e5fb44868e07cecd
Hottensiah Wanjiku <w.hottensiah96@gmail.com>
2024-06-11 09:46:09 +0300

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [images, setImages] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setLoading(true); 
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then((response) => response.json())
+            .then((data) => {
+                setImages([data.message]);
+                setLoading(false);
+            })
+        
+    }, []);
+    return(
+    <div>
+        {loading ? <p>Loading...</p> :
+            images.map((image,index) => (
+                <img key={index} src={image} alt={"A Random Dog"}
+                />
+            ))}
+    </div>
+    );
+}
+
+
+export default App;
